c4fd7105c15f12bd65c4e5c08160362e71045235
bhiaibogf <bhiaibogf@outlook.com>
2021-03-14 23:29:49 +0800

    add backup preview

--- wallpaper_engin_tool/tool.py
+++ wallpaper_engin_tool/tool.py
@@ -7,6 +7,7 @@ class Tool:
         self.__acf_parser = AcfParser(self.__params.path_to_workshop + 'appworkshop_431960.acf')
         self.__local_wallpaper_checker = LocalWallpaperChecker(self.__params)
         self.__deleted_wallpaper_checker = DeletedWallpaperChecker(self.__params.proxies)
+        self.__network_wallpaper_checker = NetworkWallpaperChecker(self.__params.proxies, self.__params.cookies)
 
     def parse_workshop_acf(self):
         """
@@ -38,10 +39,19 @@ class Tool:
         :return: None
         """
 
-        print('正在查询网页')
-
+        print('正在检查本地订阅')
         self.__deleted_wallpaper_checker.check(self.__local_wallpaper_checker.local_items)
-        print(self.__deleted_wallpaper_checker.deleted_items)
-        op = input('\t是否备份已经消失的订阅 (y/N)')
+        local_deleted = self.__deleted_wallpaper_checker.deleted_items
+        if local_deleted:
+            op = input('\t是否备份已经消失的订阅 (y/N)')
+            if op == 'y':
+                self.__local_wallpaper_checker.backup(local_deleted)
+
+        print('正在检查账户订阅')
+        self.__network_wallpaper_checker.html_downloader()
+        self.__deleted_wallpaper_checker.deleted_items = []
+        self.__deleted_wallpaper_checker.check(self.__network_wallpaper_checker.subscription)
+        network_deleted = self.__deleted_wallpaper_checker.deleted_items
+        op = input('\t是否下载已经消失的订阅的预览图 (y/N)')
         if op == 'y':
-            self.__local_wallpaper_checker.backup(self.__deleted_wallpaper_checker.deleted_items)
+            self.__network_wallpaper_checker.backup(network_deleted)
