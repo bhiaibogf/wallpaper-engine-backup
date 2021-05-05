a4c062843ccfaf21f715c35408df8a09559c882a
bhiaibogf <bhiaibogf@outlook.com>
2021-05-05 19:04:45 +0800

    use vdf model

--- wallpaper_engin_tool/parser/acf_parser.py
+++ wallpaper_engin_tool/parser/acf_parser.py
@@ -1,3 +1,9 @@
+"""
+使用 vdf 模块解析 wallpaper engine 的 acf 配置文件
+"""
+import vdf
+
+
 class AcfParser:
     """
     解析本地 acf 文件，获取已安装的/已订阅的项目
@@ -9,35 +15,18 @@ class AcfParser:
         :param path_to_file: acf 文件所在位置
         """
         with open(path_to_file) as file:
-            self.lines = file.readlines()
-        self.idx = 0
-
-    def __parse(self):
-        items = []
-        self.idx += 1
-        while True:
-            self.idx += 1
-            # 该类型的项目全部读完
-            if self.lines[self.idx] == '\t}\n':
-                break
-            items.append(self.lines[self.idx][3:-2])
-            # 跳过项目中除了编号的条目
-            while True:
-                self.idx += 1
-                if self.lines[self.idx] == '\t\t}\n':
-                    break
-        return items
+            self.__dic = vdf.load(file)
 
     def get_installed_items(self):
-        # 找到已安装壁纸的开始位置
-        while True:
-            self.idx += 1
-            text = self.lines[self.idx]
-            if text == '\t\"WorkshopItemsInstalled\"\n':
-                break
-        return self.__parse()
+        """
+        获取已安装壁纸列表
+        :return: 已安装壁纸列表
+        """
+        return list(self.__dic['AppWorkshop']['WorkshopItemsInstalled'].keys())
 
     def get_subscribed_items(self):
-        # 找到已订阅壁纸的开始位置
-        self.idx += 1
-        return self.__parse()
+        """
+        获取已订阅壁纸列表
+        :return: 已订阅壁纸列表
+        """
+        return list(self.__dic['AppWorkshop']['WorkshopItemDetails'].keys())
